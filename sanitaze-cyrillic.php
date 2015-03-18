8dfdc7d6878e8fe15f746288c64165430f360c49
Sergey Kuzmich <7501301@gmail.com>
2015-03-18 18:09:13 +0300

    Reformat code

--- sanitaze-cyrillic.php
+++ sanitaze-cyrillic.php
@@ -13,31 +13,33 @@ License: GPLv2
  * @package Sanitaze Cyrillic
  */
 
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if (!defined('WPINC')) {
+    die;
 }
 
 require_once 'functions.php';
 
-add_filter( 'wp_handle_upload_prefilter', 'sanitize_cyrillic_file_name' );  
-function sanitize_cyrillic_file_name( $file ) {
-	preg_match('%\.[^.\\\\/:*?"<>|\r\n]+$%i', $file['name'], $matches);
-	$ext = $matches[0];
-	$name = sanitaze(basename($file['name'], $ext));
-	$file['name'] = $name . $ext;
+add_filter('wp_handle_upload_prefilter', 'sanitize_cyrillic_file_name');
+function sanitize_cyrillic_file_name($file)
+{
+    preg_match('%\.[^.\\\\/:*?"<>|\r\n]+$%i', $file['name'], $matches);
+    $ext = $matches[0];
+    $name = sanitaze(basename($file['name'], $ext));
+    $file['name'] = $name . $ext;
 
     return $file;
 }
 
 add_filter('sanitize_title', 'sanitize_cyrillic_title', 10, 3);
-function sanitize_cyrillic_title($title, $raw_title, $context) {
+function sanitize_cyrillic_title($title, $raw_title, $context)
+{
     $title = urldecode($title);
-	if ( 'save' == $context ) {
-		$title = remove_accents($title);
-	}
-	$title = sanitaze($title);
+    if ('save' == $context) {
+        $title = remove_accents($title);
+    }
+    $title = sanitaze($title);
 
-	return $title;
+    return $title;
 }
 
 
