370e0816da725554400abacfae2841037501ae24
Sergey Kuzmich <accounts@kuzmi.ch>
2018-01-14 18:38:17 +0300

    Enable WordPress Coding Standart validation (#7)
    * Enable WordPress Coding Standart validation
    
    * Fix CS

--- sanitaze-cyrillic.php
+++ sanitaze-cyrillic.php
@@ -13,48 +13,40 @@ License: GPLv2
  * @package Sanitaze Cyrillic
  */
 
-if (!defined('WPINC')) {
-    die;
+if ( ! defined( 'WPINC' ) ) {
+	die;
 }
 
-add_filter('sanitize_file_name', 'sanitize_cyrillic_file_name', 10, 3);
-function sanitize_cyrillic_file_name($name, $raw_name)
-{
-    preg_match('%\.[^.\\\\/:*?"<>|\r\n]+$%i', $name, $matches);
-    $ext = $matches[0];
-    $base_name = sanitaze(basename($name, $ext));
-    return $base_name . $ext;
+add_filter( 'sanitize_file_name', 'sanitize_cyrillic_file_name', 10, 3 );
+function sanitize_cyrillic_file_name( $name, $raw_name ) {
+	preg_match( '%\.[^.\\\\/:*?"<>|\r\n]+$%i', $name, $matches );
+	$ext       = $matches[0];
+	$base_name = sanitaze( basename( $name, $ext ) );
+
+	return $base_name . $ext;
 }
 
-add_filter('sanitize_title', 'sanitize_cyrillic_title', 10, 3);
-function sanitize_cyrillic_title($title, $raw_title, $context)
-{
-    $title = urldecode($title);
-    if ('save' == $context) {
-        $title = remove_accents($title);
-    }
-    $title = sanitaze($title);
+add_filter( 'sanitize_title', 'sanitize_cyrillic_title', 10, 3 );
+function sanitize_cyrillic_title( $title, $raw_title, $context ) {
+	$title = urldecode( $title );
+	if ( 'save' === $context ) {
+			$title = remove_accents( $title );
+	}
+	$title = sanitaze( $title );
 
-    return $title;
+	return $title;
 }
 
 /**
  * @param $name
  * @return string
  */
-function sanitaze($name)
-{
+function sanitaze( $name ) {
+	$cyr = array( 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я' );
+	$lat = array( 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'zh', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'ts', 'ch', 'sh', 'sch', '', 'y', '', 'e', 'ju', 'ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'zh', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'ts', 'ch', 'sh', 'sch', '', 'y', '', 'e', 'ju', 'ya' );
 
-    $cyr = array(
-        'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
-        'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я');
+	$output = str_replace( $cyr, $lat, $name );
+	$output = preg_replace( '%[^\-_a-zA-Z0-9]%i', '-', $output );
 
-    $lat = array(
-        'a', 'b', 'v', 'g', 'd', 'e', 'e', 'zh', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'ts', 'ch', 'sh', 'sch', '', 'y', '', 'e', 'ju', 'ya',
-        'a', 'b', 'v', 'g', 'd', 'e', 'e', 'zh', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'ts', 'ch', 'sh', 'sch', '', 'y', '', 'e', 'ju', 'ya');
-
-    $output = str_replace($cyr, $lat, $name);
-    $output = preg_replace('%[^\-_a-zA-Z0-9]%i', '-', $output);
-
-    return $output;
+	return $output;
 }
